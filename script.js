5765086fa789b920cdfadd142ed3ef18b89611a6
MattBugeja <matthewabugeja@gmail.com>
2021-08-23 16:58:49 +0200

    reverted to prior commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // -----JSON Request-------
 let requestURl = "data.json";
 let request = new XMLHttpRequest();
@@ -11,24 +10,15 @@ request.onload = function () {
   // ------End JSON request------
 
   const planetMenu = document.querySelectorAll(".js-planet-menu__btn");
-
   let planetName = document.querySelector(".js-planet-name");
   const text = document.querySelector(".js-text");
   const planetImg = document.querySelector(".js-planet-img");
   const planetSurfaceImg = document.querySelector(".js-planet__surface");
   const sourceLink = document.querySelector(".js-source");
-  const homeMenu = document.querySelector(".home-menu");
-  const wrapper = document.querySelector(".wrapper");
-  let index = 0;
-
   const attributeQuantities = document.querySelectorAll(
     ".attributes__quantity"
   );
 
-<<<<<<< HEAD
-
-  const navBtn = document.querySelectorAll(".nav-item");
-||||||| 7bd76d7 (plaent choice btn  color effects work)
   const navBtn = document.querySelectorAll(".nav-item");
   const planetColors = {
     mercury: "#419EBB",
@@ -46,7 +36,7 @@ request.onload = function () {
   let index = 0; //loads Mercury as first planet;
   const desktopScreen = window.matchMedia('(min-width:1300px)');
 
-  const mobileScreen = window.matchMedia('max-width: 365px');
+  // const mobileScreen = window.matchMedia('max-width: 365px');
 
   function indexHandler(planetName) {
     index = planetInfo.findIndex(
@@ -86,124 +76,51 @@ request.onload = function () {
 
     surfaceImgToggle(index, menuChoice);
   }
-=======
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-  navBtn.forEach((btn)=>{btn.addEventListener("click",function(){
-||||||| 7bd76d7 (plaent choice btn  color effects work)
   navBtn.forEach((btn) => {
     btn.addEventListener("click", function () {
       planetName.textContent = btn.textContent;
       index = indexHandler(planetName);
       attributesHandler(index);
-=======
-  const navBtn = document.querySelectorAll(".nav-item");
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-    planetName.textContent = btn.textContent;
-      index = planetInfo.findIndex(
-        (x) => x.name.toLowerCase() === `${planetName.textContent}`
-      );
-||||||| 7bd76d7 (plaent choice btn  color effects work)
       menuChoice = null;
-=======
-  navBtn.forEach((btn)=>{btn.addEventListener("click",function(){
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-      planetImg.src = planetInfo[index].images.overview;
-||||||| 7bd76d7 (plaent choice btn  color effects work)
       pageBuilder(index, menuChoice);
-=======
-    planetName.textContent = btn.textContent;
-      index = planetInfo.findIndex(
-        (x) => x.name.toLowerCase() === `${planetName.textContent}`
-      );
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-      attributeQuantities[0].textContent = planetInfo[index].rotation;
-      attributeQuantities[1].textContent = planetInfo[index].revolution;
-      attributeQuantities[2].textContent = planetInfo[index].radius;
-      attributeQuantities[3].textContent = planetInfo[index].temperature;
-      text.textContent = planetInfo[index].overview.content;
-||||||| 7bd76d7 (plaent choice btn  color effects work)
       planetMenu.forEach((btn) => {
         btn.style.backgroundColor = "transparent";
       });
-=======
-      planetImg.src = planetInfo[index].images.overview;
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-      sourceLink.setAttribute("href", planetInfo[index].overview.source)
-      planetSurfaceImg.style.visibility = "hidden";
-  
-  
-  
-  })})
-||||||| 7bd76d7 (plaent choice btn  color effects work)
       navBtn.forEach((btn) => {
         btn.style.border = "none";
       });
       
-=======
-      attributeQuantities[0].textContent = planetInfo[index].rotation;
-      attributeQuantities[1].textContent = planetInfo[index].revolution;
-      attributeQuantities[2].textContent = planetInfo[index].radius;
-      attributeQuantities[3].textContent = planetInfo[index].temperature;
-      text.textContent = planetInfo[index].overview.content;
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-||||||| 7bd76d7 (plaent choice btn  color effects work)
       if(desktopScreen.matches){
  
       btn.style.borderTop = ` thick solid ${
         planetColors[planetName.textContent]
       }`;
-=======
-      sourceLink.setAttribute("href", planetInfo[index].overview.source)
-      planetSurfaceImg.style.visibility = "hidden";
-  
-  
-  
-  })})
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
-<<<<<<< HEAD
-||||||| 7bd76d7 (plaent choice btn  color effects work)
     }
 
       planetSurfaceImg.style.visibility = "hidden";
     });
   });
-=======
-
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
 
   planetMenu.forEach((btn) =>
     btn.addEventListener("click", function () {
-      const menuChoice = this.dataset.type;
-      planetSurfaceImg.src = planetInfo[index].images.surface;
-  
-      text.textContent = planetInfo[index][menuChoice].content;
-           sourceLink.setAttribute("href", planetInfo[index][menuChoice].source);
+      menuChoice = this.dataset.type;
+      pageBuilder(index, menuChoice);
 
-      if (menuChoice === "surface") {
-        planetSurfaceImg.style.visibility = "visible";
-        planetImg.src = planetInfo[index].images.overview;
-      } else {
-        planetSurfaceImg.style.visibility = "hidden";
-        planetImg.src = planetInfo[index].images[menuChoice];
-      }
+      planetMenu.forEach((btn) => {
+        btn.style.backgroundColor = "transparent";
+      });
 
-         })
+      btn.style.backgroundColor = `${planetColors[planetName.textContent]}`;
+    })
   );
 };
-
 
 // Hamburger Menu
 const hamburger = document.querySelector(".hamburger");
@@ -215,46 +132,3 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-||||||| 81f72e5 (planet choice btn  color effects work)
-<<<<<<< HEAD
-=======
-
-
-// small change
->>>>>>> planetchoiceeffects
-=======
->>>>>>> parent of 81f72e5 (planet choice btn  color effects work)
-||||||| 81f72e5 (planet choice btn  color effects work)
-<<<<<<< HEAD
-=======
-
-
-<<<<<<< HEAD
-// small change
->>>>>>> planetchoiceeffects
-=======
->>>>>>> parent of 81f72e5 (planet choice btn  color effects work)
-||||||| a6dc3b2 (planet choice btn  color effects work)
-// small change
-=======
->>>>>>> parent of a6dc3b2 (planet choice btn  color effects work)
-||||||| a6dc3b2 (planet choice btn  color effects work)
-// small change
-=======
->>>>>>> parent of a6dc3b2 (planet choice btn  color effects work)
-||||||| a6dc3b2 (planet choice btn  color effects work)
-// small change
-=======
->>>>>>> parent of a6dc3b2 (planet choice btn  color effects work)
-||||||| 7bd76d7 (plaent choice btn  color effects work)
-=======
-
-
->>>>>>> parent of 7bd76d7 (plaent choice btn  color effects work)
